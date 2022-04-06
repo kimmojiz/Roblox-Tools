41991cebc0cab70bde29c7bb05d8ea4393e898cd
KIMMOJI <59527466+kimmojiz@users.noreply.github.com>
2022-04-06 21:40:05 +0700

    Update Kaitan Tool.lua

--- Kaitan Tool.lua
+++ Kaitan Tool.lua
@@ -1,8 +1,8 @@
 game.StarterGui:SetCore("SendNotification", {
-      Title = title,
+      Title = "Kaitan Tool",
       Text = "Go to buy script \n https://discord.gg/4UQ7myK99P",
       Icon = "http://www.roblox.com/asset/?id=9010576467",
-      Duration = 10
+      Duration = 60 * 5
    })
 
 setclipboard("https://discord.gg/4UQ7myK99P")
